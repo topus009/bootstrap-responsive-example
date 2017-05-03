55e3cfb25b31e71117b03bc9ad35b1ac679729f8
topus009 <sj_89@mail.ru>
2017-05-04 00:38:18 +0400

    responsive w/o dropdown - glitch

---
+++ index.js
@@ -0,0 +1,11 @@
+WebFontConfig = {
+  google: { families: [ 'Open+Sans:400,300,600,700:latin,cyrillic' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})(); 
